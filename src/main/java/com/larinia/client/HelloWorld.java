b1a4034d002613fc9c18001353e380c727a1b3af
Lei Yu <leiyu@ponyo.usersys.redhat.com>
2020-05-29 12:25:49 +0100

    change the class to print and sleep

--- src/main/java/com/larinia/client/HelloWorld.java
+++ src/main/java/com/larinia/client/HelloWorld.java
@@ -1,14 +1,31 @@
 package com.larinia.client;
 
-public class HelloWorld {
+public class HelloWorld implements Runnable {
     static boolean isActive = true;
-    public static void  printMessage() {
-        while(isActive) {
+
+    public static void printMessage() {
+        while (isActive) {
             System.out.println("HelloWorld.printMessage: Hello World!");
         }
     }
 
-    public static void main (String args[]) {
-        printMessage();
+    public static void main(String args[]) {
+        //printMessage();
+        while (isActive) {
+            HelloWorld hw = new HelloWorld();
+            Thread t = new Thread(hw);
+
+            try {
+                t.sleep(2000000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            t.start();
+        }
+    }
+
+    @Override
+    public void run() {
+        System.out.println("HelloWorld.printMessage: Hello World!");
     }
 }
